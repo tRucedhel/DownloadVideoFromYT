f3a0510f9b1737bbdda066096ebb11eeecea4897
Muhammad Fadhel <77598231+tRucedhel@users.noreply.github.com>
2021-12-11 12:57:59 +0800

    Add files via upload

---
+++ DownloadYTdaripython.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+while(True) :
+    url = input("Masukkan Link :")
+
+    my_video = YouTube(url)
+    print("Apakah judul video yang didownload :")
+    print(my_video.title)
+
+    jawab = input("Y/N :")
+    if(jawab.lower() =="y") :
+        break
+
+
+my_video = my_video.streams.get_highest_resolution()
+my_video.download()
